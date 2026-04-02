495f65451fc5288d691950f8de80b2da04db85bd
GeChao <gechao.work@outlook.com>
2026-04-02 13:51:45 +0800

    update front

--- frontend/js/config.js
+++
@@ -1,9 +0,0 @@
-(function bootstrapConfig(global) {
-    global.AppModules = global.AppModules || {};
-
-    global.AppModules.config = Object.freeze({
-        BASE_URL: '/api/r1',
-        TOKEN_STORAGE_KEY: 'accessToken',
-        THEME_STORAGE_KEY: 'zhiyuan-theme'
-    });
-})(window);
